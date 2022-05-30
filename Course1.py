9f4882de651be34cb2bc32c3102019d8ea1b6b82
deepak3952 <deepak3952@gmail.com>
2022-05-31 00:31:01 +0530

    added the couse1 data

---
+++ Course1.py
@@ -0,0 +1,3 @@
+#1. Name : Deepak singh Panwar
+#2. Module Name : Introduction to Git and GitHub
+#3. Learning from the Module : Learned how to fork the github repository and how to use the pull request to push your code to the github community
